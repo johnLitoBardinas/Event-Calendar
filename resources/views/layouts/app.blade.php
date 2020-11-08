a55d89e4c4e759b212d69999955dd6af3162f00b
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-11-08 17:01:22 +0800

    Add new asset css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @livewireStyles
 </head>
 <body>
