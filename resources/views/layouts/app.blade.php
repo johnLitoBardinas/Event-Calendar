d6e3423435853b699b6c73ed51c6fec6ed60d2c9
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-11-08 17:11:57 +0800

    Revert back the asset link app.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ sprintf('%s/css/app.css', env('APP_URL')) }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
 <body>
