ccd3cc33f25f789e85f7211f176d80f973a9d52c
John Lito Bardinas <bardinasjohnlito@gmail.com>
2020-11-08 16:30:52 +0800

    Remove comments

--- app/User.php
+++ app/User.php
@@ -11,8 +11,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var array
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -20,8 +18,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
-     * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
@@ -29,8 +25,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast to native types.
-     *
-     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
